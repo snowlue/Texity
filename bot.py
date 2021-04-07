1f3c368f000ab056040b2bf40861f95793879bc3
Mark1zzz <ermack228@yandex.ru>
2021-04-07 14:47:45 +0300

    сделан каркас бота, с опозданием на день, но не суть

--- bot.py
+++ bot.py
@@ -1,53 +1,88 @@
 from secrets import API_KEY
 
-from telegram import ReplyKeyboardMarkup, Update
-from telegram.ext import CallbackContext, Filters, MessageHandler, Updater
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    MessageHandler,
+    Filters,
+    ConversationHandler,
+    CallbackContext,
+)
 
 from logger import log
 
 markup = ReplyKeyboardMarkup([['Ресурсы', 'Рынок'],
                               ['Население', 'Строительство'],
                               ['Внешняя политика']], one_time_keyboard=False)
+MENU, RESOURCES, MARKET, POPULATION, CONSTRUCTION, FOREIGN_POLICY = range(6)
 
+@log
+def menu(update: Update, context: CallbackContext):
+    update.message.reply_text("Добро пожаловать в Город {}.format(name_of_city)", reply_markup=markup)
+    return MENU
 
 @log
 def resources(update: Update, context: CallbackContext):
+    resources_markup = ReplyKeyboardMarkup([['Вернуться в меню']], one_time_keyboard=False)
     update.message.reply_text(
-        "Ваши ресурсы", reply_markup=markup)
+        "Ваши ресурсы", reply_markup=resources_markup)
+    return RESOURCES
 
 
 @log
 def market(update: Update, context: CallbackContext):
+    market_markup = ReplyKeyboardMarkup([['Вернуться в меню']], one_time_keyboard=False)
     update.message.reply_text(
-        "Рынок", reply_markup=markup)
+        "Рынок", reply_markup=market_markup)
+    return MARKET
 
 
 @log
 def population(update: Update, context: CallbackContext):
-    update.message.reply_text("Ваше население", reply_markup=markup)
+    population_markup = ReplyKeyboardMarkup([['Вернуться в меню']], one_time_keyboard=False)
+    update.message.reply_text("Ваше население", reply_markup=population_markup)
+    return POPULATION
 
 
 @log
 def construction(update: Update, context: CallbackContext):
+    construction_markup = ReplyKeyboardMarkup([['Вернуться в меню']], one_time_keyboard=False)
     update.message.reply_text(
-        "Строительство", reply_markup=markup)
+        "Строительство", reply_markup=construction_markup)
+    return CONSTRUCTION
 
 
 @log
 def foreign_policy(update: Update, context: CallbackContext):
+    foreign_policy_markup = ReplyKeyboardMarkup([['Вернуться в меню']], one_time_keyboard=False)
     update.message.reply_text(
-        "Внешняя политика", reply_markup=markup)
+        "Внешняя политика", reply_markup=foreign_policy_markup)
+    return FOREIGN_POLICY
 
 
 def run():
     updater = Updater(API_KEY)
     dp = updater.dispatcher
 
-    dp.add_handler(MessageHandler(Filters.text("Ресурсы"), resources))
-    dp.add_handler(MessageHandler(Filters.text("Рынок"), market))
-    dp.add_handler(MessageHandler(Filters.text("Население"), population))
-    dp.add_handler(MessageHandler(Filters.text("Строительство"), construction))
-    dp.add_handler(MessageHandler(Filters.text("Внешняя политика"), foreign_policy))
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', menu)],
+        states={
+            MENU: [MessageHandler(Filters.regex('^(Ресурсы)$'), resources),
+                   MessageHandler(Filters.regex('^(Рынок)$'), market),
+                   MessageHandler(Filters.regex('^(Население)$'), population),
+                   MessageHandler(Filters.regex('^(Строительство)$'), construction),
+                   MessageHandler(Filters.regex('^(Внешняя политика)$'), foreign_policy)],
+            RESOURCES: [MessageHandler(Filters.regex('^(Вернуться в меню)$'), menu)],
+            MARKET: [MessageHandler(Filters.regex('^(Вернуться в меню)$'), menu)],
+            POPULATION: [MessageHandler(Filters.regex('^(Вернуться в меню)$'), menu)],
+            CONSTRUCTION: [MessageHandler(Filters.regex('^(Вернуться в меню)$'), menu)],
+            FOREIGN_POLICY: [MessageHandler(Filters.regex('^(Вернуться в меню)$'), menu)],
+        },
+        fallbacks=[CommandHandler('cancel', menu)],
+    )
+
+    dp.add_handler(conv_handler)
 
     updater.start_polling()
     updater.idle()
